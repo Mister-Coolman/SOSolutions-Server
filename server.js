cee966eb4732f97650fc0f82df251d1b8d0a4950
Arjun Rangarajan <arjunrangarajan2008@gmail.com>
2026-05-27 20:54:38 -0400

    Changed voice ID for tts

--- server.js
+++ server.js
@@ -638,7 +638,7 @@ async function playIntroMessage(session) {
   const text = parts.join(' ');
 
   const elevenRes = await fetch(
-    'https://api.elevenlabs.io/v1/text-to-speech/JBFqnCBsd6RMkjVDRZzb',
+    'https://api.elevenlabs.io/v1/text-to-speech/7EzWGsX10sAS4c9m9cPf',
     {
       method: 'POST',
       headers: {
